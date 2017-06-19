ca6ee9276b056f2dad6400727e5bfeca7df31974
Takuma Inaba <inataku@MacBook-Pro-3.local>
2017-06-20 01:27:49 +0900

    DIVE14 exam 06200127

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  config.action_mailer.default_url_options = {host: 'https://gentle-springs-84973.herokuapp.com/'}
+  config.action_mailer.default_url_options = {host: 'https://mighty-fortress-24779.herokuapp.com/'}
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
