8ba50c797ca67df4a6f1d740ffc4ba48406f53cc
Takuma Inaba <inataku@MacBook-Pro-3.local>
2017-06-19 17:36:46 +0900

    DIVE14 exam completed2

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -12,10 +12,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def twitter
-    @user = user.find_for_twitter_oauth(request.env["omniauth.auth"], current_user)
+    @user = User.find_for_twitter_oauth(request.env["omniauth.auth"], current_user)
 
     if @user.persisted?
-      set_flashmessage(:notice, :success, kind: "Twitter") if is_navigational_format?
+      set_flash_message(:notice, :success, kind: "Twitter") if is_navigational_format?
       sign_in_and_redirect @user, event: :authentication
     else
       session["devise.twitter_data"] = request.env["omniauth.auth"].except("extra")
